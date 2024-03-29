da3922f3f3e6c9ac74011a9fd30f5158e49f890b
Alexandre Mahdhaoui <alexandre.mahdhaoui@gmail.com>
2024-03-29 17:18:32 +0100

    fix: improve arch/install.sh

--- arch/install.sh
+++ arch/install.sh
@@ -11,7 +11,9 @@ wifi_setup() {
   echo "Successfully connected to wifi station ${STATION}"
 }
 
-# timedatectl Europe/Berlin
+set_timezone() {
+  timedatectl set-timezone Europe/Berlin
+}
 
 # PARTITION DISK
 ## Select DISK
@@ -91,7 +93,7 @@ setup_swap() {
 
 # run_pacstrap
 run_pacstrap() {
-  pacstrap -K /mnt base linux linux-firmware base-devel man-db man-pages texinfo networkmanager iwd bash vim
+  pacstrap -K /mnt base linux linux-firmware base-devel man-db man-pages texinfo networkmanager iwd bash tmux vim intel-ucode git
   # removed lvm2
 }
 
@@ -100,7 +102,63 @@ run_genfstab() {
   genfstab -U /mnt | tee -a /mnt/etc/fstab
 }
 
-wifi_setup
+setup_root() {
+  echo "Please add [ 'systemd', 'keyboard', 'sd-vconsole', 'sd-encrypt' ] to /etc/mkinitcpio.conf"
+  echo "Example: HOOKS=(base systemd autodetect microcode modconf kms keyboard sd-vconsole block sd-encrypt filesystems fsck)"
+  printf "Press ENTER wehn ready to edit the file..."
+  read YOLO
+  arch-chroot /mnt vim /etc/mkinitcpio.conf
+  cat <<EOF | arch-chroot /mnt
+echo "rd.luks.name=$(blkid | grep "${ROOTPART}" | sed 's/^.*UUID"//;s/".*//')" | tee /boot/loader/entries/arch.conf
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+hwclock --systohc
+EOF
+
+  echo "Please enter your hostname: "
+  read NEW_HOSTNAME
+  cat <<EOF | arch-chroot /mnt
+echo "${NEW_HOSTNAME}" | tee /etc/hostname
+echo "en_US.UTF-8 UTF-8" | tee -a /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" | tee /etc/locale.conf
+EOF
+
+  arch-chroot /mnt mkinitcpio -P
+}
+
+setup_user() {
+  echo "Please enter name for your user:"
+  read NEW_USER
+  echo "Confirm username '${NEW_USER}' (y/n): "
+  read CONFIRM
+  if [[ "$CONFIRM" != "y" ]]; then setup_user; return;fi
+  arch-chroot /mnt useradd -m -G wheel -s /usr/bin/bash "${NEW_USER}"
+}
+
+set_user_passwd() {
+  echo "You will set the password for '${NEW_USER}'. Press ENTER when ready."
+  read YOLO
+  arch-chroot /mnt passwd ${NEW_USER}
+  echo "Confirm new password (y/n): "
+  read CONFIRM
+  if [[ "$CONFIRM" != "y" ]]; then set_user_passwd; return;fi
+}
+
+setup_bootloader() {
+  arch-chroot /mnt bootctl install
+}
+
+lets_reboot() {
+  echo "Are you ready to reboot? (y/n) "
+  read CONFIRM
+  if [[ "$CONFIRM" != "y" ]]; then echo "Please run 'lets_reboot' when ready to umount and reboot your machine."; return;fi
+ 
+  umount -R /mnt
+  reboot
+}
+
+echo "TODO: remove wifi_setup from this file and add it to the README.md"
+set_timezone
 disk_select
 disk_unsecure_erase
 disk_partition
@@ -112,5 +170,9 @@ echo "TODO: Update mirrorlist"
 run_pacstrap
 run_genfstab
 
-echo "Run 'arch-chroot /mnt' and continue the setup"
+setup_root
+setup_user
+set_user_passwd
+setup_bootloader
 
+lets_reboot
