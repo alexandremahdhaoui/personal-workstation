ef9afd62f8d8f4e4c88c34a26f6bcec74d05bf8a
Alexandre Mahdhaoui <alexandre.mahdhaoui@gmail.com>
2024-03-29 20:27:27 +0100

    fix: remove redundant cat

--- arch/config.sh
+++ arch/config.sh
@@ -5,31 +5,31 @@ cache_config() {
 }
 
 run_pacman() {
-  cat /tmp/arch_config.yaml  | yq '.pacman[]' | xargs pacman -S --noconfirm
+  yq '.pacman[]' /tmp/arch_config.yaml | xargs pacman -S --noconfirm
 }
 
 run_flatpak() {
   # install remotes
-  cat /tmp/arch_config.yaml | yq '.flatpak.remote' | sed "s/\://" | xargs -I{} flatpak remote-add --if-not-exists "{}"
+  yq '.flatpak.remote' /tmp/arch_config.yaml | sed "s/\://" | xargs -I{} flatpak remote-add --if-not-exists "{}"
   
   # install from remotes
-  for remote in $(cat /tmp/arch_config.yaml | yq '.flatpak.remote | keys | .[]'); do
-    cat /tmp/arch_config.yaml | yq ".flatpak.${remote}[]" |  xargs -I{} echo flatpak install "${remote}" "{}"
+  for remote in $(yq '.flatpak.remote | keys | .[]' /tmp/arch_config.yaml); do
+    yq ".flatpak.${remote}[]" |  xargs -I{} echo flatpak install "${remote}" "{}"
   done
   
   # install flatpaks from flatpakref
-  cat /tmp/arch_config.yaml | yq '.flatpak.ref[]' | xargs -I{} flatpak install -y "{}"
+  yq '.flatpak.ref[]' /tmp/arch_config.yaml | xargs -I{} flatpak install -y "{}"
 }
 
 run_gsettings() {
-  cat /tmp/arch_config.yaml | yq '.gsettings[]' | xargs -I{} echo gsettings "{}"
+  yq '.gsettings[]' /tmp/arch_config.yaml | xargs -I{} echo gsettings "{}"
 }
 
 run_files() {
-  cat /tmp/arch_config.yaml  | yq '.files.home | keys | .[]' | xargs dirname | xargs -I{} echo "${HOME}/{}" |  xargs mkdir -p
+  yq '.files.home | keys | .[]' /tmp/arch_config.yaml | xargs dirname | xargs -I{} echo "${HOME}/{}" |  xargs mkdir -p
 
-  for configpath in $(cat /tmp/arch_config.yaml  | yq '.files.home | keys | .[]'); do
-    cat /tmp/arch_config.yaml | yq ".files.home.\"${configpath}\"" | tee "${HOME}/${configpath}"
+  for configpath in $(yq '.files.home | keys | .[]' /tmp/arch_config.yaml); do
+    yq ".files.home.\"${configpath}\"" | tee "${HOME}/${configpath}"
   done
 }
 
